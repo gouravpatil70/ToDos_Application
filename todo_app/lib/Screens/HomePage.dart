3ac95e9a73d3d6ff52c7c241fb9a3b25b8291de2
gourav <gouravpatil075@gmail.com>
2023-08-20 05:26:16 +0530

    Added Bottom navigator desion & Some changes in other files related to BottomNavigator button

--- todo_app/lib/Screens/HomePage.dart
+++ todo_app/lib/Screens/HomePage.dart
@@ -12,17 +12,38 @@ class HomePage extends StatefulWidget{
 
 class _HomePageState extends State<HomePage>{
 
+  int currentSelectedId = 0;
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: AppColors.appBackgroundColor,
       appBar: Components.customAppBarMethod("ToDo's", true, calenderShowMethod),
-      
+      bottomNavigationBar: Components.customBottomNavigationBar(currentSelectedId,changeDisplayPage),
     );
   }
 
-  calenderShowMethod(){
 
+  // Method for BottomNavigation Bar
+  changeDisplayPage(int value){
+    setState(() {
+      currentSelectedId = value;
+    });
+    switch (value) {
+      case 0:
+        navigateToHomePage();
+        break;
+      case 1:
+        navigateToEditeToDosPage();
+        break;
+    }
   }
+
+  calenderShowMethod(){}
+
   defaultEmptyMethod(){}
+
+  navigateToEditeToDosPage(){}
+
+  navigateToHomePage(){}
 }
