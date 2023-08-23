cbd23c1b2c2a388a0eb983ee6da68a2b9e6eedc8
gourav <gouravpatil075@gmail.com>
2023-08-24 04:59:43 +0530

    Added Buttons, Added Save Button & Some other Functionality, New Colors added

--- todo_app/lib/Screens/AddTaskPage.dart
+++ todo_app/lib/Screens/AddTaskPage.dart
@@ -16,6 +16,8 @@ class _AddTaskState extends State<AddTask> {
   int currentIndex = 1;
   final GlobalKey<FormState> _key = GlobalKey();
   String currentPriority = 'High';
+  String noteTitle = '';
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -50,20 +52,22 @@ class _AddTaskState extends State<AddTask> {
                 padding: const EdgeInsets.fromLTRB(40.0, 40.0, 40.0, 10.0),
                 child: Components.textWidget('Task Title'),
               ),
+
               Padding(
                 padding: const EdgeInsets.fromLTRB(40.0, 10.0, 40.0, 10.0),
-                child: TextFormField(
-                    validator: (input){
-                      if(input!.isEmpty){
-                        return 'Please provide title here';
-                      }
-                      return null;
-                    },
-                    maxLines: 8,
-                    maxLength: 100,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
+                child: Components.textFormFieldWidget(noteTitle),
+              ),
+
+
+              // Buttons 
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Components.buttonWidget(AppColors.saveButtonColor,'Save', onSaveMethod),
+                    Components.buttonWidget(AppColors.deleteButtonColor,'Delete', onDeleteMethod),
+                  ],
                 ),
               )
             ],
@@ -98,5 +102,47 @@ class _AddTaskState extends State<AddTask> {
     });
   }
 
+  onSaveMethod(){
+      if(_key.currentState!.validate()){
+        _key.currentState!.save();
+        showDialogBox('Success','Data Saved');
+      }
+  }
+  onDeleteMethod(){}
+
+  showDialogBox(String title, String content){
+    return showDialog(
+      context: context, 
+      builder: (BuildContext context){
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(9.0)
+          ),
+          title: Text(
+            title
+          ),
+          content: Text(
+            content
+          ),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              }, 
+              style: TextButton.styleFrom(
+                backgroundColor: AppColors.appPrimaryColor,
+              ),
+              child: const Text(
+                'Ok',
+                style: TextStyle(
+                  color: AppColors.textWhiteColor,
+                ),
+              ),
+            )
+          ],
+        );
+      }
+    );
+  }
   defaultEmptyMethod(){}
 }
