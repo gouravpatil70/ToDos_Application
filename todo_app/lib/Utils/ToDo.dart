a1ab5be3adf0ef64171c9f7a76b5a8fc91ef852c
gourav <gouravpatil075@gmail.com>
2023-08-25 04:59:10 +0530

    Added database libraries in pubspec & Created two files DatabaseHelper & ToDo for the getter & setter

---
+++ todo_app/lib/Utils/ToDo.dart
@@ -0,0 +1,54 @@
+class ToDo{
+
+  var _id;
+  var _title;
+  var _priority;
+  var _date;
+
+  // Getters
+  int get id => _id;
+
+  String get title => _title;
+
+  String get priority => _priority;
+
+  String get date => _date;
+
+
+  // Setters
+  set id(int newId){
+    _id = newId;
+  }
+
+  set title(String newtitle){
+    _title = newtitle;
+  }
+
+  set priority(String newPriority){
+    _priority = newPriority;
+  }
+
+  set date(String neDate){
+    _date = neDate;
+  }
+  
+
+  // toMap
+  Map<String,dynamic> convertToMapObject(){
+    Map<String,dynamic> map = <String,dynamic>{};
+    map['id'] = _id;
+    map['title'] = _title;
+    map['priority'] = _priority;
+    map['date'] = _date;
+    return map;
+  }
+
+  // From Map Object
+  ToDo.fromMap(Map<String,dynamic> map){
+    _id = map['id'];
+    _title = map['title'];
+    _priority = map['priority'];
+    _date = map['date'];
+  }
+  
+}
